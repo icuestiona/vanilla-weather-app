410128bbfdb9653ce9bbf5346a2f3a4e0ce314b7
Soraya <icuestiona@gmail.com>
2022-06-16 13:36:24 +0200

    Added showFahrenheitTemperature and showCelsiusTemperature js

--- src/script.js
+++ src/script.js
@@ -56,3 +56,28 @@ function callSubmit(event) {
   let city = document.querySelector(`#search-input`).value;
   searchCity(city);
 }
+function showFahrenheitTemperature(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector(`#temperature`);
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+function showCelsiusTemperature(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector(`#temperature`);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
+
+let fahrenheitLink = document.querySelector(`#fahrenheit-link`);
+fahrenheitLink.addEventListener(`click`, showFahrenheitTemperature);
+
+let celsiusLink = document.querySelector(`#celsius-link`);
+celsiusLink.addEventListener(`click`, showCelsiusTemperature);
